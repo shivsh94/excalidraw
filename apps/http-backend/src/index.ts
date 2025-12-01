1bb325884c95feb8829061d8a4a0ff58e47721f6
shivsh94 <shivamsharma23jan@gmail.com>
2025-12-01 16:04:07 +0530

    feat: implement JWT authentication with signup and signin endpoints
    - Added JWT_SECRET configuration for token signing.
    - Created signup endpoint to simulate user registration.
    - Implemented signin endpoint to generate JWT token for authenticated users.
    - Added middleware for token verification on protected routes.
    - Configured TypeScript settings for the backend application.

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middlewale } from "./middleware";
+
+const app = express();
+
+app.post("/signup", (req, res) => {
+    //db call
+
+    res.json({
+        userId: 123
+    })
+})
+
+app.post("/signin", (req, res ) => {
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+})
+
+app.post("/room", middlewale, (req, res) => {
+    //db call
+
+    res.json({
+        roomId: 123
+    })
+})
+
+app.listen(3001);
